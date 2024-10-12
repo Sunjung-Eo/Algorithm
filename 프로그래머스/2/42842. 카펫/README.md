3901d31e43b260ffc537ad58464f0e5c944ab7dc
Sunjung-Eo <sunjung.eo@gmail.com>
2024-10-12 09:52:40 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 74.9 MB, 시간: 0.05 ms
+메모리: 77.9 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 12일 09:43:05
+2024년 10월 12일 09:52:38
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,19 +1,18 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int area = brown + yellow;
-        int h = 3;
-        int w = area / h;
         
-        for (int i = h; i < w; i++) {
-            h = i;
-            w = area / h;
-            
-            if (area % h == 0 && yellow == (w-2) * (h-2)) {
-               break; 
+        for (int h = 3; h <= area / 3; h++) {
+            if (area % h == 0) {
+                int w = area / h;
+                
+                if (yellow == (w - 2) * (h - 2)) {
+                    int[] answer = {w, h};
+                    return answer;
+                }
             }
         }
         
-        int[] answer = {w, h};
-        return answer;
+        return null;
     }
 }
