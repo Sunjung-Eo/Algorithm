d045d1761fb5f3a5f21439f23937b513c35002af
Sunjung-Eo <sunjung.eo@gmail.com>
2025-05-21 09:28:26 +0900

    [Silver IV] Title: 덱, Time: 224 ms, Memory: 19476 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter  bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(br.readLine());
+        Deque<Integer> que = new ArrayDeque<Integer>();
+        
+        while (n-- > 0) {
+        	String[] str = br.readLine().split(" ");
+        	if (str[0].equals("push_front")) {
+        		que.addFirst(Integer.parseInt(str[1]));
+        	}
+        	
+        	if (str[0].equals("push_back")) {
+        		que.addLast(Integer.parseInt(str[1]));
+        	}
+        	
+        	if (str[0].equals("pop_front")) {
+        		if (!que.isEmpty()) {
+        			sb.append(que.pollFirst()).append("\n");
+        		} else {
+        			sb.append("-1").append("\n");
+        		}
+        	}
+        	
+        	if (str[0].equals("pop_back")) {
+        		if (!que.isEmpty()) {
+        			sb.append(que.pollLast()).append("\n");
+        		} else {
+        			sb.append("-1").append("\n");
+        		}
+        	}
+        	
+        	if (str[0].equals("size")) {
+        		sb.append(que.size()).append("\n");
+        	}
+        	
+        	if (str[0].equals("empty")) {
+        		if (que.isEmpty()) {
+        			sb.append("1").append("\n");
+        		} else {
+        			sb.append("0").append("\n");
+        		}
+        	}
+        	
+        	if (str[0].equals("front")) {
+        		if (!que.isEmpty()) {
+            		sb.append(que.peekFirst()).append("\n");
+        		} else {
+        			sb.append("-1").append("\n");
+        		}
+        		
+        	}
+        	
+        	if (str[0].equals("back")) {
+        		if (!que.isEmpty()) {
+            		sb.append(que.peekLast()).append("\n");
+        		} else {
+        			sb.append("-1").append("\n");
+        		}
+        	}
+        }
+        
+        bw.append(sb.toString());
+        bw.flush();
+        bw.close(); br.close();
+    }
+}
